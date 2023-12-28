bd26fe2da5e8809c1009e58c3b71209427ad35c8
Brian Takita <brian.takita@gmail.com>
2023-12-27 23:43:30 -0500

    update dependencies

--- src/ctx/browser__ctx.ts
+++ src/ctx/browser__ctx.ts
@@ -1,10 +1,10 @@
 /// <reference lib="dom" />
 import { blog__ctx__new } from '@btakita/domain--any--blog'
-import { type Ctx } from 'ctx-core/be'
+import { type Ctx_wide_T } from 'ctx-core/be'
 export const browser__ctx =
 	window.browser__ctx ??= blog__ctx__new()
 declare global {
 	interface Window {
-		browser__ctx:Ctx<''>
+		browser__ctx:Ctx_wide_T<''>
 	}
 }
