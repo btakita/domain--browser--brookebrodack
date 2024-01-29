936f01607e094180c65fd2e28d26d66fe7ff4f30
Brian Takita <brian.takita@gmail.com>
2024-01-28 21:23:58 -0500

    ./ctx: browser_ctx: ∋ blog__ctx__new→blog_ctx__new

--- ctx/browser_ctx.ts
+++ ctx/browser_ctx.ts
@@ -1,8 +1,8 @@
 /// <reference lib="dom" />
-import { blog__ctx__new } from '@btakita/domain--any--blog'
+import { blog_ctx__new } from '@btakita/domain--any--blog'
 import { type Ctx_wide_T } from 'ctx-core/be'
 export const browser_ctx =
-	window.browser_ctx ??= blog__ctx__new()
+	window.browser_ctx ??= blog_ctx__new()
 declare global {
 	interface Window {
 		browser_ctx:Ctx_wide_T<''>
